197a4195bca3b61b30d840b8b9c22542b7944b70
Nikhil Verma <63592210+nikhilverma17@users.noreply.github.com>
2023-08-09 10:19:59 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import React,{useState} from 'react';
 
 function App() {
+   const [fullName,setFullName]=useState({
+    fName:"",
+    lName:""
+  });
+  function handleChange(event){
+    const newValue=event.target.value;
+    const inputName=event.target.name;
+    setFullName(prevValue => {
+      if(inputName==="fname")
+      {return{
+        fName:newValue,
+        lName:prevValue.lName
+      }}
+      else if (inputName==="lname")
+      {return{
+        fName:prevValue.fName,
+        lName:newValue
+      }}
+    })
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+     <h1>hello {fullName.fName} {fullName.lName} </h1>
+     <input onChange={handleChange} name="fname" type="text" placeholder="First Name"value={fullName.fName}></input>
+     <input onChange={handleChange} name="lname" type="text" placeholder="Last Name"value={fullName.lName}></input>
+     <button>submit</button>
     </div>
   );
 }
