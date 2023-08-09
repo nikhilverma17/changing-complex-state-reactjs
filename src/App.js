e0ab934136436874cbf188f3de99d2e836ac14f2
Nikhil Verma <63592210+nikhilverma17@users.noreply.github.com>
2023-08-09 14:26:02 +0530

    updated code

--- src/App.js
+++ src/App.js
@@ -1,35 +1,51 @@
 import logo from './logo.svg';
 import './App.css';
-import React,{useState} from 'react';
+import React, { useState } from 'react';
 
 function App() {
-   const [fullName,setFullName]=useState({
-    fName:"",
-    lName:""
+  const [fullName, setFullName] = useState({
+    fName: "",
+    lName: "",
+    email:""
   });
-  function handleChange(event){
-    const newValue=event.target.value;
-    const inputName=event.target.name;
+
+  function handleChange(event) {
+
+    const { value, name } = event.target
+
     setFullName(prevValue => {
-      if(inputName==="fname")
-      {return{
-        fName:newValue,
-        lName:prevValue.lName
-      }}
-      else if (inputName==="lname")
-      {return{
-        fName:prevValue.fName,
-        lName:newValue
-      }}
+      if (name === "fname") {
+        return {
+          fName: value,
+          lName: prevValue.lName,
+          email:prevValue.email
+ 
+        }
+      }
+      else if (name === "lname") {
+        return {
+          fName: prevValue.fName,
+          lName: value,
+          email:prevValue.email
+        }
+      }else if(name==="email"){
+        return{
+          fName: prevValue.fName,
+          lName: prevValue.lName,
+          email:value
+        }
+      }
     })
   }
-  
+
   return (
     <div className="container">
-     <h1>hello {fullName.fName} {fullName.lName} </h1>
-     <input onChange={handleChange} name="fname" type="text" placeholder="First Name"value={fullName.fName}></input>
-     <input onChange={handleChange} name="lname" type="text" placeholder="Last Name"value={fullName.lName}></input>
-     <button>submit</button>
+      <h1>hello {fullName.fName} {fullName.lName} </h1>
+      <input onChange={handleChange} name="fname" type="text" placeholder="First Name" value={fullName.fName}></input>
+      <input onChange={handleChange} name="lname" type="text" placeholder="Last Name" value={fullName.lName}></input>
+      <p>{fullName.email}</p>
+      <input onChange={handleChange} name="email" type="text" placeholder="Email" value={fullName.email}></input>
+      <button>submit</button>
     </div>
   );
 }
